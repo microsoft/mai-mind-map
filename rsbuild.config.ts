e259d456df0412d6554ffd371cce8f7930a69d16
jianliwei <jianliwei@microsoft.com>
2024-09-05 16:23:58 +0800

    listen process.env.PORT first

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
+function normalizePort(val) {
+  const port = Number.parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 export default defineConfig({
   plugins: [pluginReact()],
+  server: {
+    port: normalizePort(process.env.PORT || '3000'),
+  },
 });
