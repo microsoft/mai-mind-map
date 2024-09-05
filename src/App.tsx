7dc6af55864ed2cb5cd8c5c8aeee51b7aab11c27
jianliwei <jianliwei@microsoft.com>
2024-09-05 14:33:38 +0800

    setup repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>MSAI mind map</h1>
+      <p>Turn every thing into mind map.</p>
+    </div>
+  );
+};
+
+export default App;
