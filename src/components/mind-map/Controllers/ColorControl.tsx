73309bb02b4363985da66024bf9f915edc044bfb
jianliwei <jianliwei@microsoft.com>
2024-09-22 17:51:47 +0800

    Local persistence for render option

--- src/components/mind-map/Controllers/ColorControl.tsx
+++ src/components/mind-map/Controllers/ColorControl.tsx
@@ -1,14 +1,21 @@
 import { css } from '@base/styled';
-import { FC } from 'react';
-import { Curve, HybridLines, RoundedFoldingLine } from '../../icons/icons';
+import { Payload } from '@root/components/mind-map/render/model/interface';
+import { RawNode } from '@root/components/mind-map/render/node/interface';
+import { MindMapState } from '@root/components/state/mindMapState';
+import { MindMapStateType } from '@root/components/state/mindMapState';
+import { throttle } from 'lodash';
+import { FC, Fragment, useContext } from 'react';
 import { ColorMode } from '../render/hooks/constants';
 
+const SColorModeControlBox = css`
+  margin: 5px 0 15px;
+`;
 const SColorModeControl = css`
   display: flex;
   align-items: center;
   line-height: 20px;
   gap: 10px;
-  margin: 5px 0 15px;
+  margin-bottom: 10px;
   & > .label {
     width: 80px;
     text-align: right;
@@ -30,14 +37,93 @@ const SColorModeControl = css`
     }
   }
 `;
+const SColorPane = css`
+  background: linear-gradient(90deg, red, yellow, green, cyan, blue, magenta);
+  height: 20px;
+`;
+const SDefaultPane = css`
+  background: linear-gradient(90deg, #0172dc, #ecf2fb, #fff);
+  height: 20px;
+`;
+
+const SColoringBtn = css`
+  margin-top: 5px;
+  display: inline-block;
+  padding: 5px 10px;
+  border: 1px solid #1893ff;
+  cursor: pointer;
+  background-color: aliceblue;
+  &:hover {
+    background-color: #badfff;
+  }
+`;
+
+const rootColor = '#212429';
+const colors = [
+  '#E7A400',
+  '#E67505',
+  '#EB4824',
+  '#E82C41',
+  '#D7257D',
+  '#B91CBF',
+  '#9529C2',
+  '#6D37CD',
+  '#4A43CB',
+  '#2052CB',
+  '#0067BF',
+  '#0C74A1',
+  '#1A7F7C',
+  '#288A56',
+  '#379539',
+  '#63686E',
+  '#666666',
+];
+function shuffle<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function ColoringNode(
+  node: RawNode<Payload>,
+  color: string,
+  modifyNodePayload: (nodeId: string, payload: Payload) => void,
+) {
+  modifyNodePayload(
+    node.id,
+    Object.assign({}, node.payload, { hilight: color }),
+  );
+  node.children?.forEach((child, ind) => {
+    ColoringNode(child, color, modifyNodePayload);
+  });
+}
+
+function ColoringMindMap(tresState: MindMapStateType | null) {
+  if (!tresState) return;
+  const colorArray = shuffle([...colors]);
+  const root = tresState.mindMapData;
+  const modifyNodePayload = tresState.modifyNodePayload;
+  modifyNodePayload(
+    root.id,
+    Object.assign({}, root.payload, { hilight: rootColor }),
+  );
+  root.children?.forEach((node, ind) => {
+    let index = ind % colors.length;
+    ColoringNode(node, colorArray[index], modifyNodePayload);
+  });
+}
+const throttleColoringMindMap = throttle(ColoringMindMap, 1000);
 
 export const ColorModeControl: FC<{
   colorMode: ColorMode;
   setColorMode: (val: ColorMode) => void;
 }> = (props) => {
   const { colorMode, setColorMode } = props;
+  const treeState = useContext(MindMapState);
   return (
-    <div>
+    <div className={SColorModeControlBox}>
       <div className={SColorModeControl}>
         <span className="label">ColorMode:</span>
         {(
@@ -70,6 +156,24 @@ export const ColorModeControl: FC<{
           );
         })}
       </div>
+      {colorMode === ColorMode.DEFAULT ? (
+        <div className={SDefaultPane}></div>
+      ) : (
+        <Fragment>
+          <div className={SColorPane}></div>
+          <div>
+            <button
+              className={SColoringBtn}
+              onClick={() => {
+                throttleColoringMindMap(treeState);
+              }}
+            >
+              {' '}
+              Coloring my MindMap{' '}
+            </button>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 };
--- src/components/mind-map/render/hooks/useRenderOption.ts
+++ src/components/mind-map/render/hooks/useRenderOption.ts
@@ -2,10 +2,56 @@ import { useCallback, useState } from 'react';
 import { Direction } from '../layout/flex-tree/hierarchy';
 import { ColorMode, LinkMode } from './constants';
 
-let direction: Direction = 'H';
-let scaleValue = 1;
-let linkModeValue: LinkMode = LinkMode.HYBRID;
-let colorModeValue: ColorMode = ColorMode.DEFAULT;
+interface OptionStore {
+  direction: Direction;
+  scale: number;
+  linkMode: LinkMode;
+  colorMode: ColorMode;
+}
+
+const storeKey = 'mind_map_option';
+
+const defaultOption: OptionStore = {
+  direction: 'H',
+  scale: 1,
+  linkMode: LinkMode.HYBRID,
+  colorMode: ColorMode.CUSTOM,
+};
+function loadOption(): OptionStore {
+  let option: OptionStore = Object.assign({}, defaultOption);
+  const optionBase64 = localStorage.getItem(storeKey);
+  if (optionBase64) {
+    try {
+      option = JSON.parse(atob(optionBase64));
+    } catch (e) {}
+    if (
+      option.direction &&
+      option.scale &&
+      option.linkMode &&
+      option.colorMode
+    ) {
+      return option;
+    }
+    option = Object.assign({}, defaultOption);
+    return option;
+  }
+  return option;
+}
+
+const optionStore = loadOption();
+let direction = optionStore.direction;
+let scaleValue = optionStore.scale;
+let linkModeValue = optionStore.linkMode;
+let colorModeValue = optionStore.colorMode;
+function saveOption() {
+  const option = {
+    direction,
+    scale: scaleValue,
+    linkMode: linkModeValue,
+    colorMode: colorModeValue,
+  };
+  localStorage.setItem(storeKey, btoa(JSON.stringify(option)));
+}
 
 export function useRenderOption() {
   const [dir, setDirFun] = useState<Direction>(direction);
@@ -15,18 +61,22 @@ export function useRenderOption() {
   const setDir = useCallback((dir: Direction) => {
     setDirFun(dir);
     direction = dir;
+    saveOption();
   }, []);
   const setScale = useCallback((scale: number) => {
     setScaleFun(scale);
     scaleValue = scale;
+    saveOption();
   }, []);
   const setLinkMode = useCallback((linkMode: LinkMode) => {
     setLinkModeFun(linkMode);
     linkModeValue = linkMode;
+    saveOption();
   }, []);
   const setColorMode = useCallback((colorMode: ColorMode) => {
     setColorModeFun(colorMode);
     colorModeValue = colorMode;
+    saveOption();
   }, []);
 
   return {
