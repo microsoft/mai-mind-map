96edc2a4adcaf5bb770489c722c75177245d1cc5
Jingyao Zhou <jingyaozhou@microsoft.com>
2024-09-23 15:27:40 +0800

    fix: fix decorator build error

--- office-addin/webpack.config.js
+++ office-addin/webpack.config.js
@@ -32,15 +32,29 @@ module.exports = async (env, options) => {
     module: {
       rules: [
         {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-typescript"],
-              plugins: [["@babel/plugin-proposal-decorators", { version: "2023-11" }]],
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [["@babel/preset-env"]],
+              },
             },
-          },
+            {
+              loader: "ts-loader",
+            },
+          ],
+        },
+        {
+          test: /\.m?jsx?$/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [["@babel/preset-env"]],
+              },
+            },
+          ],
         },
         {
           test: /\.html$/,
