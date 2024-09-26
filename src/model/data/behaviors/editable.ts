60407865fb99dfd8d38fcae451730e1db0d92704
Scott Wei <leescott@microsoft.com>
2024-09-26 15:36:20 +0800

    Return both value and op on update

--- src/model/data/behaviors/editable.ts
+++ src/model/data/behaviors/editable.ts
@@ -1,34 +1,38 @@
 import { Maybe, just, nothing } from "@root/model/ot-doc/maybe";
 import { BehaviorDef } from "../behavior";
 import { $Var } from "../higher-kinded-type";
-import { $Op, $PrimOp, Prim, op } from "../op";
+import { $Op, $PrimOp, Prim, op as $op } from "../op";
 import { reduceDict, reduceStruct } from "../struct";
 import { Eq } from "./eq";
 import { Preset } from "./preset";
 
-export type Editable<T = $Var> = {
-  update: (op: (a: T) => $Op<T>) => (a: T) => Maybe<T>;
-};
+export type Update<T> = (
+  updater: (a: T) => $Op<T>
+) => (a: T) => { value: Maybe<T>; op: $Op<T> };
 
-const withUpdate = <T>(
-  update: (op: (a: T) => $Op<T>) => (a: T) => Maybe<T>,
-): Editable<T> => ({ update });
+export type Editable<T = $Var> = { update: Update<T> };
 
-const updatePrim = <T extends Prim>(op: (a: T) => $PrimOp<T>) => (v: T): Maybe<T> => {
-  const { o, n } = op(v);
-  return o === v ? just(n) : nothing();
-}
+const withUpdate = <T>(update: Update<T>): Editable<T> => ({ update });
+
+const withUpdatePrim = <T extends Prim>(): Editable<T> =>
+  withUpdate<T>(((updater) => (v) => {
+    const op = updater(v);
+    const { o, n } = op;
+    return { value: o === v ? just(n) : nothing(), op };
+  }) as Update<T>);
 
 const editable: BehaviorDef<Editable, Eq & Preset> = {
-  $string: () => withUpdate(updatePrim),
-  $number: () => withUpdate(updatePrim),
-  $boolean: () => withUpdate(updatePrim),
+  $string: withUpdatePrim,
+  $number: withUpdatePrim,
+  $boolean: withUpdatePrim,
   $array:
     ({ eq }) =>
     () =>
-      withUpdate((f) => (arrOld) => {
-        const { i: ins, d: del } = f(arrOld);
-        if (del.length === 0 && ins.length === 0) return just(arrOld);
+      withUpdate((updater) => (arrOld) => {
+        const op = updater(arrOld);
+        const { i: ins, d: del } = op;
+        if (del.length === 0 && ins.length === 0)
+          return { value: just(arrOld), op };
         const arrNew = [...arrOld];
         for (const { i: idx, a: arr } of del) {
           if (
@@ -36,50 +40,70 @@ const editable: BehaviorDef<Editable, Eq & Preset> = {
             idx < 0 ||
             !eq(arr)(arrNew.slice(idx, idx + arr.length))
           )
-            return nothing();
+            return { value: nothing(), op };
           arrNew.splice(idx, arr.length);
         }
         for (const { i: idx, a: arr } of ins) {
-          if (idx > arrNew.length || idx < 0) return nothing();
+          if (idx > arrNew.length || idx < 0) return { value: nothing(), op };
           arrNew.splice(idx, 0, ...arr);
         }
-        return just(arrNew);
+        return { value: just(arrNew), op };
       }),
-  $dict: () => ({ update, preset, eq }) => withUpdate((f) => (dictOld) => reduceDict(f(dictOld), (m, opVal, key) => {
-    if (m.$ === 'Nothing' || !opVal || typeof key !== 'string') return m;
-    const valOld = dictOld[key] ?? preset;
-    const mValNew = update(() => op(opVal))(valOld);
-    if (mValNew.$ === 'Nothing') return nothing();
-    if (!eq(dictOld[key])(mValNew.v)) {
-      if (m.v === dictOld) {
-        m.v = { ...dictOld };
-      }
-      if (eq(preset)(mValNew.v)) {
-        delete m.v[key];
-      } else {
-        m.v[key] = mValNew.v;
-      }
-    }
-    return m;
-  }, just(dictOld))),
+  $dict:
+    () =>
+    ({ update, preset, eq }) =>
+      withUpdate((f) => (dictOld) => {
+        const op = f(dictOld);
+        const value = reduceDict(
+          op,
+          (m, opVal, key) => {
+            if (m.$ === "Nothing" || !opVal || typeof key !== "string")
+              return m;
+            const valOld = dictOld[key] ?? preset;
+            const { value: mValNew, op } = update(() => $op(opVal))(valOld);
+            if (mValNew.$ === "Nothing") return nothing();
+            if (!eq(dictOld[key])(mValNew.v)) {
+              if (m.v === dictOld) {
+                m.v = { ...dictOld };
+              }
+              if (eq(preset)(mValNew.v)) {
+                delete m.v[key];
+              } else {
+                m.v[key] = mValNew.v;
+              }
+            }
+            return m;
+          },
+          just(dictOld)
+        );
+        return { value, op };
+      }),
   $struct: () => (sttDoc) =>
-    withUpdate(
-      (f) => (sttOld) => reduceStruct(sttOld, (m, valOld, key) => {
-        const sttOp = f(sttOld);
-        const opKey = key as keyof typeof sttOp;
-        if (m.$ === 'Nothing' || !sttOp[opKey] || typeof opKey !== 'string') return m;
-        const valNew = sttDoc[key].update(() => op(sttOp[opKey] as any))(valOld);
-        if (valNew.$ === 'Nothing') return nothing();
-        if (!sttDoc[key].eq(valOld)(valNew.v)) {
-          if (m.v === sttOld) {
-            m.v = { ...sttOld };
+    withUpdate((updater) => (sttOld) => {
+      const op = updater(sttOld);
+
+      const value = reduceStruct(
+        sttOld,
+        (m, valOld, key) => {
+          const opKey = key as keyof typeof op;
+          if (m.$ === "Nothing" || !op[opKey] || typeof opKey !== "string")
+            return m;
+          const { value: mValNew } = sttDoc[key].update(() =>
+            $op(op[opKey] as any)
+          )(valOld);
+          if (mValNew.$ === "Nothing") return nothing();
+          if (!sttDoc[key].eq(valOld)(mValNew.v)) {
+            if (m.v === sttOld) {
+              m.v = { ...sttOld };
+            }
+            m.v[key] = mValNew.v;
           }
-          m.v[key] = valNew.v;
-        }
-        return m;
-      }, just(sttOld))
-    ),
+          return m;
+        },
+        just(sttOld)
+      );
+      return { value, op };
+    }),
 };
 
 export default editable;
-
